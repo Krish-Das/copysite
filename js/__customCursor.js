312fa946f3f8eea78c5171d34b733ed88af3ead0
Krish-Das <krishdasatgames@gmail.com>
2023-03-10 21:36:15 +0530

    --ADDED: Custom cursor is added

---
+++ js/__customCursor.js
@@ -0,0 +1,11 @@
+const cursor = document.createElement("div");
+cursor.className = "cursor";
+document.body.appendChild(cursor);
+document.body.style.overflowX = "hidden";
+
+function repositionCursor(e) {
+  cursor.style.top = `${e.pageY}px`;
+  cursor.style.left = `${e.pageX}px`;
+}
+
+window.addEventListener("mousemove", (e) => repositionCursor(e));
